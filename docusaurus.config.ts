ce7a827f139966c6b65b4d2db6a4b0903ea500a5
Lars Reuter <44064366+shlareut@users.noreply.github.com>
2025-08-12 10:16:37 +0200

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -73,7 +73,7 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'Lars Docusaurus Test',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
@@ -83,7 +83,7 @@ const config: Config = {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Doku',
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
